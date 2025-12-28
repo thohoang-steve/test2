6fabcacce56cc4f4a5908208270186ad7a452dcc
thohoang-steve <thohoang.steve@gmail.com>
2025-12-28 20:42:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from docx import Document
 
 # --- CẤU HÌNH TRANG ---
 st.set_page_config(page_title="SEO Content Researcher", layout="wide")
-st.markdown("""<style>.main {background-color: #f4f6f9;} h1 {color: #2c3e50;} .stButton>button {width: 100%; border-radius: 5px; font-weight: bold;}</style>""", unsafe_allow_html=True)
+st.markdown("""<style>.main {background-color: #f4f6f9;} h2 {color: #FC6E20;} .stButton>button {width: 100%; border-radius: 5px; font-weight: bold;}</style>""", unsafe_allow_html=True)
 
 # --- SESSION STATE ---
 if 'results' not in st.session_state: st.session_state['results'] = None
@@ -176,5 +176,6 @@ else:
         pd.DataFrame(res['rec_list'], columns=['Recommended H2']).to_excel(writer, sheet_name='Outline Recommend', index=False)
     buf_xls.seek(0)
     with b2: st.download_button("Tải Excel", buf_xls, "SEO_Data.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", type="primary")
+
 
 
